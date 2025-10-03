c5da7c13d5cb65d180540b61e650911b3ba0e989
LintangTriandini <triandinilintang@gmail.com>
2025-10-03 10:28:24 +0700

    PRAK PBW

---
+++ PRAK 3/LaraPress/resources/views/kontak.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Kontak</title>
+</head>
+<body>
+    <h1>Kontak Kami</h1>
+    <p>Email: triandinilintang@gmail.com</p>
+    <p>Telepon: +62 8786 4820 010</p>
+
+    <p><a href="/">Kembali ke Halaman Utama</a></p>
+    <p><a href="/tentang-kami">Tentang Kami</a></p>
+</body>
+</html>
